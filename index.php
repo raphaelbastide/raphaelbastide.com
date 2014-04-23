4c0ab3a33c7b4e9b047fd2bbf0a087dda7acc89d
Raphaël Bastide <raphael.bastide@gmail.com>
2014-04-23 21:54:47 +0200

    basic information

--- index.php
+++ index.php
@@ -6,28 +6,16 @@
     <title>Raphaël Bastide</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="public/font/stylesheet.css">
   </head>
   <body>
-
-<?php
-
-require_once('lib/Git.php');
-$repo = Git::open('');
-
-echo "Tags:<br>";
-$tags = $repo->list_tags();
-foreach ($tags as $tag) {
-    echo $tag."<br>";
-}
-
-echo "<br>Branches:<br>";
-$branches = $repo->list_branches();
-foreach ($branches as $branch) {
-    echo $branch;
-}
-
-?> 
-    <p><a href="mailto:bonjour@raphaelbastide.com">email</a> <a href="http://eepurl.com/o-nJj">newsletter</a> <a href="http://twitter.com/raphaelbastide">Twitter</a> <a href="https://github.com/raphaelbastide/">GitHub</a><p>
+    <header>
+    <p>Raphaël Bastide</p>
+    <p>Graphic designer, coder, artist.<br/>Lives and Works in Paris.<br/>Uses, design and modify free software.</p>
+    <p><a href="mailto:bonjour@raphaelbastide.com">Email</a> - <a href="http://eepurl.com/o-nJj">Newsletter</a> - <a href="http://twitter.com/raphaelbastide">Twitter</a> - <a href="https://github.com/raphaelbastide/">GitHub</a><p>
+    </header>
+    <div class="images">
+    </div>
   </body>
 </html>
