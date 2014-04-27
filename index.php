05b7bfba0f3c93794280d39158b2faa515de4104
Raphaël Bastide <raphael.bastide@gmail.com>
2014-04-27 22:49:59 +0200

    Slideshow + basic php script to list image

--- index.php
+++ index.php
@@ -15,7 +15,31 @@
     <p>Graphic designer, coder, artist.<br/>Lives and Works in Paris.<br/>Uses, design and modify free software.</p>
     <p><a href="mailto:bonjour@raphaelbastide.com">Email</a> - <a href="http://eepurl.com/o-nJj">Newsletter</a> - <a href="http://twitter.com/raphaelbastide">Twitter</a> - <a href="https://github.com/raphaelbastide/">GitHub</a><p>
     </header>
-    <div class="images">
+    <p class="recent"><a href="http://raphaelbastide.com/esad2014/">Workshop ESAD Valence</a></p>
+    <div class="images cycle-slideshow" 
+      data-cycle-loader="true"
+      data-cycle-random="true"
+      data-cycle-log="false"
+      data-cycle-fx="fadeout"
+      data-cycle-swipe="true"
+      data-cycle-swipe-fx="scrollHorz"
+      data-cycle-pause-on-hover="true"
+      data-cycle-timeout="5000"
+      data-cycle-speed="100">
+      <?
+      $directory = "public/projects/";
+      $images = glob($directory."{*.jpg,*.jpeg,*.gif,*.png,*.svg}", GLOB_BRACE);
+      natsort($images);
+      foreach ($images as $img) {
+        $info = pathinfo($img);
+        $imageName =  basename($img,'.'.$info['extension']);
+        list($width, $height, $type, $attr)= getimagesize($img);
+        echo "<img src='$img' class='cycle-next ".$imageName."' width='$width' height='$height' /> \n";
+      }
+      ?>
     </div>
+    <script src='public/js/jquery.js'></script>
+    <script src='public/js/cycle.js'></script>
+    <script src='public/js/main.js'></script>
   </body>
 </html>
