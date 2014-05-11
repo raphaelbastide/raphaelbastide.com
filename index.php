83e8cac0f1269ad3207f92922a4a94eb96fbf2b7
Raphaël Bastide <raphael.bastide@gmail.com>
2014-05-12 00:04:50 +0200

    More lines

--- index.php
+++ index.php
@@ -11,11 +11,50 @@
   </head>
   <body>
     <header>
-    <p>Raphaël Bastide</p>
-    <p>Graphic designer, coder, artist.<br/>Lives and Works in Paris.<br/>Uses, design and modify free software.</p>
-    <p><a href="mailto:bonjour@raphaelbastide.com">Email</a> - <a href="http://eepurl.com/o-nJj">Newsletter</a> - <a href="http://twitter.com/raphaelbastide">Twitter</a> - <a href="https://github.com/raphaelbastide/">GitHub</a><p>
+      <p>Raphaël Bastide</p>
+      <p>Art, code and graphic design.<br/>Lives and Works in Paris.<br/>Uses, design and modify free software and alternative tools.</p>
+      <p><a href="mailto:bonjour@raphaelbastide.com">Email</a> - <a href="http://eepurl.com/o-nJj">Newsletter</a> - <a href="http://twitter.com/raphaelbastide">Twitter</a> - <a href="https://github.com/raphaelbastide/">GitHub</a><p>
     </header>
-    <p class="recent"><a href="http://raphaelbastide.com/esad2014/">Workshop ESAD Valence</a></p>
+    <main>
+      <section class="recent" id="usemodify">
+        <p><a href="http://usemodify.com/">Use & Modify</a> is a libre font list I curate.</p>
+      </section>
+      <section class="recent" id="ofont">
+        <p><a href="http://ofont.net/">ofont</a> is an online tool for font classification. <a href="https://github.com/raphaelbastide/ofont">Download</a> the sources on GitHub.</p>
+      </section>
+      <section class="recent" id="esad-2014">
+        <p>Workshop ESAD Valence <a href="http://raphaelbastide.com/esad2014/">« Objets versionnés »</a>.</p>
+        <time>2014 April</time>
+      </section>
+      <section id="lgm-usemodify">
+        <p>Talk at Libre Graphics Meeting <a href="#usemodify">Use & Modify</a>.</p>
+        <time>2014 March</time>
+      </section>
+      <section id="revisable-1">
+        <p>Personal exhibition <a href="http://raphaelbastide.com/revisables/">Révisable B</a> at iMAL, Brussels.</p>
+        <time>2013 August</time>
+      </section>
+      <section id="branch">
+        <p>Personal exhibition <a href="http://raphaelbastide.com/branch/">BRANCH</a> at De La Charge, Brussels.</p>
+        <time>2013 February</time>
+      </section>
+      <section id="volumes">
+        <p>Exhibition “Volumes” with <a href="http://kevinbray.biz/">Kevin Bray</a> at 22 Rue Muller, Paris.</p>
+        <time>2012 November</time>
+      </section>
+      <section id="parallax">
+        <p>Dirty Web design workshop <a href="http://raphaelbastide.com/parallax/">Parallax</a> at Festival de l’Affiche et du Graphisme in Chaumont, France.</p>
+        <time>2012 June</time>
+      </section>
+      <section id="fabien-mousse">
+        <p>Creation of the persona <a href="http://fabien-mousse.fr/">Fabien Mousse</a>.</p>
+        <time>2008 December</time>
+      </section>
+      <section id="niei">
+        <p>Creation of the persona <a href="http://niei.neocities.org/">Network Identity Experiment Institute</a>.</p>
+        <time>2008 February</time>
+      </section>
+    </main>
     <div class="images cycle-slideshow" 
       data-cycle-loader="true"
       data-cycle-random="true"
