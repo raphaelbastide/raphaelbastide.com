b8c0a4adb0b8f655230883d4d6693a786fdd815f
Raphaël Bastide <raphael.bastide@gmail.com>
2014-05-26 16:57:58 +0200

    Maybe more flexible this way

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,7 @@ function doPlumb(from, to){
     mainLinks.connect({
       source: from, 
       target: to,
-      anchor:["LeftMiddle", "TopLeft"]
+      anchor:[.1, .2, 0, .5]
     });
 
     $('.cycle-slideshow').on('cycle-after', function() {
@@ -27,8 +27,7 @@ function doPlumb(from, to){
   });
 }
 
-
-$( '.cycle-slideshow' ).on( 'cycle-after', function() {
+function connect(){
   var currImgId = $('.cycle-slide.cycle-slide-active').attr('id'),
       // Regex pattern to escape "-"+"digit" as in "branch-2"" : /[-]\d+\b/g
       currTxtId = currImgId.replace(/[-]\d+\b/g,'').replace('-img',''),
@@ -38,8 +37,10 @@ $( '.cycle-slideshow' ).on( 'cycle-after', function() {
 
   if(currImgId.length > 0 && currTxtId.length > 0){
     console.log(currTxtId, cleanImgId)
-    setTimeout(function(){ doPlumb(currTxtId,$(".images"));},100);
-
-    
+    setTimeout(function(){ doPlumb(currTxtId,$(".images"));},100)
   }
+  
+}
+$( '.cycle-slideshow' ).on( 'cycle-after', function() {
+  connect();
 });
